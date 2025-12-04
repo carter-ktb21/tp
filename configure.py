66e6ddc9be473b373d9388bdfe182ecbbcd59854
fihn14 <cadenboss2@gmail.com>
2025-12-04 16:28:47 -0500

    Mac Dev Env (#49)
    * Mac Dev Env
    
    * most of epona is scaled
    
    * Fine tuning build iso script for mac
    
    * Camera Fix
    
    fixed yer camera bubby
    
    * Update .claude/settings.local.json
    
    ---------
    
    Co-authored-by: Sven Elmendorp <31472764+oga-sven@users.noreply.github.com>

--- configure.py
+++ configure.py
@@ -759,7 +759,7 @@ config.libs = [
             Object(MatchingFor(ALL_GCN), "d/d_insect.cpp"),
             Object(MatchingFor(ALL_GCN), "d/actor/d_a_obj_ss_base.cpp"),
             Object(MatchingFor(ALL_GCN), "d/actor/d_a_player.cpp"),
-            Object(NonMatching, "d/d_camera.cpp"),
+            Object(Modded, "d/d_camera.cpp"),  # Boofener: Modified for DELTA_TIME scaling
             Object(MatchingFor(ALL_GCN), "d/d_envse.cpp"),
             Object(MatchingFor(ALL_GCN), "d/d_file_select.cpp"),
             Object(MatchingFor(ALL_GCN), "d/d_file_sel_warning.cpp"),
@@ -1726,7 +1726,7 @@ config.libs = [
     ActorRel(MatchingFor(ALL_GCN), "d_a_vrbox"),
     ActorRel(NonMatching, "d_a_vrbox2"),
     ActorRel(MatchingFor(ALL_GCN), "d_a_arrow"),
-    ActorRel(MatchingFor(ALL_GCN), "d_a_boomerang"),
+    ActorRel(Modded, "d_a_boomerang"),  # Boofener: Modified for DELTA_TIME scaling
     ActorRel(MatchingFor(ALL_GCN), "d_a_crod"),
     ActorRel(MatchingFor(ALL_GCN), "d_a_demo00"),
     ActorRel(MatchingFor(ALL_GCN), "d_a_disappear"),
@@ -1942,7 +1942,7 @@ config.libs = [
     ActorRel(MatchingFor(ALL_GCN), "d_a_e_zs"),
     ActorRel(MatchingFor(ALL_GCN), "d_a_formation_mng"),
     ActorRel(MatchingFor(ALL_GCN), "d_a_guard_mng"),
-    ActorRel(NonMatching, "d_a_horse"),
+    ActorRel(Modded, "d_a_horse"),  # Boofener: Modified for DELTA_TIME scaling
     ActorRel(NonMatching, "d_a_hozelda"),
     ActorRel(MatchingFor(ALL_GCN), "d_a_izumi_gate"),
     ActorRel(MatchingFor(ALL_GCN), "d_a_kago"),
